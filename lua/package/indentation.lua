6ca7af0eebe7d5b73714a2a5256bb4184fb3290f
Ole-Jakob Olsen <ole.jakob.olsen@gmail.com>
2024-12-29 02:36:48 +0100

    Refactor split indentation from statusbar

--- lua/package/indentation.lua
+++ lua/package/indentation.lua
@@ -1,67 +1,23 @@
 return {
-  "kevinhwang91/nvim-ufo",
-  dependencies = {
-    "kevinhwang91/promise-async",
-    {
-      "luukvbaal/statuscol.nvim",
-      config = function()
-        local builtin = require("statuscol.builtin")
-        require("statuscol").setup({
-          relculright = true,
-          segments = {
-            {
-              sign = { namespace = { "diagnostic/signs" }, maxwidth = 1, auto = false },
-              click = "v:lua.ScSa"
-            },
-            { text = { builtin.foldfunc, " " }, click = "v:lua.ScFa" },
-            { text = { builtin.lnumfunc },      click = "v:lua.ScLa", },
-            {
-              sign = { namespace = { ".*" }, maxwidth = 1, colwidth = 1, auto = false, wrap = true },
-              click = "v:lua.ScSa"
-            },
-            { text = { " " } },
-          },
-          ft_ignore = { "neo-tree" },
-          bt_ignore = { "terminal" },
-        })
-      end,
-    },
-    {
-      {
-        'echasnovski/mini.indentscope',
-        version = '*',
-        config = function()
-          require('mini.indentscope').setup({
-            draw = {
-              delay = 20,
-              animation = require('mini.indentscope').gen_animation.none()
-            }
-          })
-        end
-      },
-    },
-    {
-      "lukas-reineke/indent-blankline.nvim",
-      main = "ibl",
-      opts = {
-
-        indent = { char = "╎" },
-        scope = { enabled = false } -- using mini.indentscope instead
-      },
-    }
+  {
+    'echasnovski/mini.indentscope',
+    version = '*',
+    config = function()
+      require('mini.indentscope').setup({
+        draw = {
+          delay = 20,
+          animation = require('mini.indentscope').gen_animation.none()
+        }
+      })
+    end
   },
-  config = function()
-    vim.o.foldcolumn = "1"
-    vim.o.foldlevel = 99
-    vim.o.foldlevelstart = 99
-    vim.o.foldenable = true
-    vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    opts = {
 
-    ---@diagnostic disable-next-line: missing-fields
-    require('ufo').setup({
-      provider_selector = function()
-        return { "treesitter", "indent" }
-      end,
-    })
-  end,
+      indent = { char = "╎" },
+      scope = { enabled = false }   -- using mini.indentscope instead
+    },
+  }
 }
