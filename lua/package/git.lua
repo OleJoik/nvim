94bda5a5e7c1e4b9469aafa36a23c77a3718108e
Ole-Jakob Olsen <ole.jakob.olsen@gmail.com>
2025-01-01 15:36:06 +0100

    Lazygit when using nvim-remote

--- lua/package/git.lua
+++ lua/package/git.lua
@@ -1,7 +1,12 @@
 return { {
   "kdheepak/lazygit.nvim",
   config = function()
-    vim.g.lazygit_floating_window_scaling_factor = 0.99
+    vim.g.lazygit_floating_window_scaling_factor = 0.95
+    vim.cmd [[
+      if has('nvim') && executable('nvr')
+        let $GIT_EDITOR = "nvr -cc split --remote-wait +'set bufhidden=wipe'"
+      endif
+    ]]
   end
 },
   {
