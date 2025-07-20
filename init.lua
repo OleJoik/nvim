9998df92123dc07231f4b0e388d0701d172010c9
Ole-Jakob Olsen <ole.jakob.olsen@gmail.com>
2025-07-20 13:44:44 +0200

    bugfix at phanteks

--- init.lua
+++ init.lua
@@ -5,3 +5,6 @@ require("package_manager")
 
 require("terminal")
 require("colors").set_colors()
+
+-- must happen after fzf is installed with package manager
+require("telescope").load_extension("fzf")
